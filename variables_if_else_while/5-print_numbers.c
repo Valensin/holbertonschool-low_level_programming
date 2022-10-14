7e0b9619bf58b5ced3ad85f342a4742f2365810f
Valensin <valensin@gmail.com>
2022-10-14 11:25:44 -0400

    Adjusted stuff

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,29 @@
+/*
+ * Author: Onil
+ * File: 5-print_numbers.c
+ */
+
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-int main()
+
+/**
+ * main - prints all single digit numbers
+ * of base 10 starting from 0, followed by a new line.
+ *
+ * Return: Always 0
+ */
+
+	int main(void)
 {
 	int x = 0;
-	 
+
 	while (x < 10)
-	{	
+	{
 		printf("%i", x);
-		
-		x++;	
- 	} 
+
+		x++;
+	}
 	printf("\n");
 	return (0);
 }
