c148158063c51bb195550cd86d646c98219c9e76
Valensin <valensin@gmail.com>
2022-10-19 10:11:46 -0400

    Libraries

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,11 +5,13 @@
 
 #include "main.h"
 /**
- * main - Write a function that prints
+ * print_alphabet - Write a function that prints
  * the alphabet, in lowercase, followed by a new line.
  * void print_alphabet(void)
  * Return: print_alphabet(void)
+ * print_alphabet: prints alphabet
  */
+
 
 void print_alphabet(void)
 {
