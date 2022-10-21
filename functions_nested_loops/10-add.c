2dd39e474eebefc30bce747962166aaa93793ab0
Valensin <onilr31@gmail.com>
2022-10-21 21:06:43 +0000

    a +b file

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+/*
+* Author: Onil
+* File: 10-add.c
+*/
+#include "main.h"
+
+/**
+* add - Adds two values
+* @number1: The first number to be computed
+* @number2: The second number to be computed
+*
+* Return: Integer value
+*/
+int add(int number1, int number2)
+{
+	return (number1 + number2);
+}
