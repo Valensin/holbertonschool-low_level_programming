b2bd6a11f02fcc1335ac50a9e3304bd7a23bc292
Valensin <onilr31@gmail.com>
2022-10-22 18:09:19 +0000

    dealing with functions & updated files

--- more_functions_nested_loops/7-main.c
+++ more_functions_nested_loops/7-main.c
@@ -1,13 +1,15 @@
 /**
  * main - check the code
- *
+ * @n: set of values
  * Return: Always 0.
  */
+void print_diagonal(int n);
 int main(void)
+
 {
-    print_diagonal(0);
-    print_diagonal(2);
-    print_diagonal(10);
-    print_diagonal(-4);
-    return (0);
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+	return (0);
 }
--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -31,7 +31,7 @@ void print_diagonal(int n)
 			_putchar(92);
 
 			_putchar('\n');
-        }
-    }
+		}
+	}
 }
 
