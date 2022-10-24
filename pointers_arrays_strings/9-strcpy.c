12ad827f571978e14323673b9a345b96d14f494e
Valensin <onilr31@gmail.com>
2022-10-24 22:39:02 +0000

    Updated files

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,20 +1,12 @@
-/*
- * Author: Onil
- * File: 9-strcpy.c
- */
-
 #include "main.h"
 
 /**
- * _strcpy - it copies the string pointed to,
- * by src to buffer pointed to by dest
- *
- * @src: source of value
- * @dest: destination of copy
- *
- * Return: pointer to dest
- */
-
+  * _strcpy - Copy a string
+  * @dest: Destination value
+  * @src: Source value
+  *
+  * Return: the pointer to dest
+  */
 char *_strcpy(char *dest, char *src)
 {
     int a;
