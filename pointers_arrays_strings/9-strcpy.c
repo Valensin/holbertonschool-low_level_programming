5a4d6e3e825a3b1f231393a8eb0bf6ed0a275eee
Valensin <onilr31@gmail.com>
2022-10-24 22:44:47 +0000

    Updated files

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -13,15 +13,15 @@
   */
 char *_strcpy(char *dest, char *src)
 {
-    int a;
+	int a;
 
-    for (a = 0; src[a] != '\0'; a++)
-    {
-        dest[a] = src[a];
-    }
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
 
-    dest[a++] = '\0';
+	dest[a++] = '\0';
 
-    return (dest);
+	return (dest);
 }
 
