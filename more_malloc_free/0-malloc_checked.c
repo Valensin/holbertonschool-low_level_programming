463a41523f47611bee5955caf71d1cbfe115f71b
Valensin <Onilr31@gmail.com>
2022-10-28 21:22:18 -0400

    Created & Updated Files

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+/*
+ * Author: Onil
+ * File: 0-malloc_checked.c
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to be allocated.
+ *
+ * Return: A pointer to the allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *x = malloc(b);
+
+	if (x == NULL)
+		exit(98);
+
+	return (x);
+}
---
+++ more_malloc_free/main.h
@@ -0,0 +1,5 @@
+void *malloc_checked(unsigned int b);
+
+int _putchar(char c);
+
+
