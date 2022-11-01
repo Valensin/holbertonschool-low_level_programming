9cfeab083da52a769fc03d0fb25fdeaa02845db7
Valensin <Onilr31@gmail.com>
2022-11-01 18:35:16 -0400

    Updating files

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,13 +1,8 @@
-/**
- * Desc: Header file containing prototypes for all functions
- * used in the 0x0E-function_pointers directory.
- */
-
+#ifndef HEAD_
+#define HEAD_
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
-
 void array_iterator(int *array, size_t size, void (*action)(int));
-
 int int_index(int *array, int size, int (*cmp)(int));
-
+#endif
