e921dde6820383dc9e5e08aa24a7c79499b3dd40
Valensin <Onilr31@gmail.com>
2022-11-01 18:26:05 -0400

    Updating files

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,9 +1,9 @@
-#include <stdlib.h>
-
 /**
  * Desc: Header file containing prototypes for all functions
  * used in the 0x0E-function_pointers directory.
  */
+
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 
